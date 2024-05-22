1c6aadde4fb3b8e795299bd7f1b8baf7723c6134
Саша Паршинцев <aleksandr-par@mail.ru>
2024-05-22 14:59:50 +0700

    create views and add TagViewSet

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from food.models import Tag, Ingredient, Recipe, RecipeIngredient
+from .serializers import (
+    TagSerializer,
+    IngredientSerializer,
+    RecipeSerializer,
+    RecipeIngredientSerializer,
+)
+
+class TagViewSet(ListCreateDestroyViewSet):
+    """ViewSet для работы с тегами."""
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    # permission_classes
+    # lookup_field = 'slug'
+    # filter_backends = (filters.SearchFilter,)
+    # search_fields = ('name',)
