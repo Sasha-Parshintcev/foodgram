a6cfa95302656fe8450137457346d46195ff7940
Саша Паршинцев <aleksandr-par@mail.ru>
2024-05-22 15:55:18 +0700

    urls  FollowViewSet

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,7 +4,8 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     TagViewSet,
     IngredientViewSet,
-    RecipeViewSet
+    RecipeViewSet,
+    FollowViewSet
     # RecipeIngredientViewSet
 )
 
@@ -13,8 +14,6 @@ router_v1 = DefaultRouter()
 router_v1.register('tags', TagViewSet, basename='tags')
 router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
 router_v1.register('recipes', RecipeViewSet, basename='recipes')
-# router_v1.register(
-#     'recipes/(?P<id>[^/.]+)',
-#     ReviewViewSet, basename='recipes'
-# )
+router_v1.register('subscribe', FollowViewSet, basename='subscribe')
+
 
