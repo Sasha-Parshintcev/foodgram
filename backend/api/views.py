99db15cc91508ec513fba8d3cdf8a5be8536e401
Саша Паршинцев <aleksandr-par@mail.ru>
2024-05-22 15:02:32 +0700

    add IngredientViewSet

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 
-from food.models import Tag, Ingredient, Recipe, RecipeIngredient
+from food.models import Tag, Ingredient, Recipe
 from .serializers import (
     TagSerializer,
     IngredientSerializer,
-    RecipeSerializer,
-    RecipeIngredientSerializer,
+    RecipeSerializer
 )
 
 class TagViewSet(ListCreateDestroyViewSet):
@@ -16,3 +15,15 @@ class TagViewSet(ListCreateDestroyViewSet):
     # lookup_field = 'slug'
     # filter_backends = (filters.SearchFilter,)
     # search_fields = ('name',)
+
+
+class IngredientViewSet(ListCreateDestroyViewSet):
+    """ViewSet для работы с ингредиентами."""
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    # permission_classes
+    # lookup_field = 'slug'
+    # filter_backends = (filters.SearchFilter,)
+    # search_fields = ('name',)
+
+
