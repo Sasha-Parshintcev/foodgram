bdee49bde0cd073cbf3fd686f509b596da786e51
Саша Паршинцев <aleksandr-par@mail.ru>
2024-05-23 17:11:51 +0700

    settings

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -36,14 +36,14 @@ ALLOWED_HOSTS = 'localhost', '127.0.0.1'
 # Application definition
 
 INSTALLED_APPS = [
-    'food.apps.FoodConfig',
-    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'food.apps.FoodConfig',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
