aa28b5fa4fe6a57e041091b237ac9f12caee110b
Саша Паршинцев <aleksandr-par@mail.ru>
2024-05-24 14:27:31 +0700

    urls static and media settings

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
@@ -5,3 +6,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-token-auth/', views.obtain_auth_token),
 ]
+
+if settings.DEBUG:
+    urlpatterns = (
+        urlpatterns
+        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
