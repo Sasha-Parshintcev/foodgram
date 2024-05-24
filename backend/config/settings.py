fecae96d6763eeea5dc1629673acf68e60462c4a
Саша Паршинцев <aleksandr-par@mail.ru>
2024-05-24 14:21:02 +0700

    rename foodgram_backend in config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'foodgram_backend.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -79,7 +79,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
