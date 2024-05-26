5e7f274cc75835e52931e23aecccee8f5031cde6
Саша Паршинцев <aleksandr-par@mail.ru>
2024-05-26 13:59:54 +0700

    fix User subscription

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,7 +6,7 @@ from .views import UserViewSet
 
 api_v1 = DefaultRouter()
 api_v1.register('users', UserViewSet, basename='users')
-# router_v1.register(
+# api_v1.register(
 #     'users/(?P<id>[^/.]+)/subscribe',
 #     FollowViewSet, basename='subscribe'
 # )
@@ -14,7 +14,7 @@ api_v1.register('users', UserViewSet, basename='users')
 urlpatterns = [
     path('', include(api_v1.urls)),
     # path('api-token-auth/', views.obtain_auth_token),
-    path('auth/', include('djoser.urls')),
+    # path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
 
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,63 @@
+from django.shortcuts import get_object_or_404
 from djoser import views as djoser_views
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from users.models import User
-from .serializers import UserSerializer
+from users.models import User, Subscription
+from .serializers import UserSerializer, SubscriptionSerializer
 
 
 class UserViewSet(djoser_views.UserViewSet):
     """Вьюсет для работы с пользователями."""
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+    @action(['get'], detail=False)
+    def me(self, request, *args, **kwargs):
+
+        return super().me(request, *args, **kwargs)
+
+    @action(['post', 'delete'],
+            detail=True,
+            permission_classes=(IsAuthenticated,))
+    def subscribe(self, request, id):
+        subscribing = get_object_or_404(User, pk=id)
+
+        if request.method == 'POST':
+            serializer = SubscriptionSerializer(
+                data={'user': request.user.pk,
+                      'subscribing': subscribing.pk},
+                context={'request': request})
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        subscription = Subscription.objects.filter(user=request.user,
+                                                   subscribing=subscribing)
+        if subscription.exists():
+            subscription.delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        return Response({'errors': 'Вы не были подписаны ранее!'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    @action(['get'],
+            detail=False,
+            permission_classes=(IsAuthenticated,))
+    def subscriptions(self, request):
+        queryset = User.objects.filter(
+            subscribing__user=request.user).prefetch_related('recipes')
+        pages = self.paginate_queryset(queryset)
+        serializer = SubscriptionSerializer(pages,
+                                            many=True,
+                                            context={'request': request})
+
+        return self.get_paginated_response(serializer.data)
 
 
 # from django.shortcuts import render
